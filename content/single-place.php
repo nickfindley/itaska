a344de9e24b403f59cd347fd06d4b7ffd669f130
Nick Findley <newadmin@Nicks-MacBook-Pro.local>
2020-11-11 07:42:46 -0600

    Adjusted hours and added minority owned businesses

--- content/single-place.php
+++ content/single-place.php
@@ -35,7 +35,13 @@ $start_date = date( 'Y-m-d', strtotime( '2019-11-29' ) );
 $end_date = date( 'Y-m-d', strtotime( '2019-12-31' ) );
 $today = date ( 'Y-m-d' );
 $today = date( 'Y-m-d', strtotime( $today ) );
-if ( get_field( 'dutchtown_cid' ) || get_field( 'cherokee_street' ) || ( get_field( 'dutchtown_passport' ) )  && ( $today >= $start_date ) && ( $today <= $end_date ) ) : ?>
+if (
+    get_field( 'dutchtown_cid' ) || 
+    get_field( 'cherokee_street' ) ||
+    ( get_field( 'dutchtown_passport' )  && ( $today >= $start_date ) && ( $today <= $end_date ) ) ||
+    get_field( 'minority_owned' )
+) :
+?>
 <div class="place-banner">
     <div class="place-banner-container">
         <?php if ( get_field( 'dutchtown_cid' ) ) : ?>
@@ -47,6 +53,35 @@ if ( get_field( 'dutchtown_cid' ) || get_field( 'cherokee_street' ) || ( get_fie
         <?php if ( get_field( 'dutchtown_passport' ) ) : if ( $today >= $start_date && $today <= $end_date ) : ?>
         <p><i class="fas fa-fw fa-passport"></i> Participating <a href="/passport/">Dutchtown Passport</a> location</p>
         <?php endif; endif; ?>
+        <?php
+            if ( get_field( 'minority_owned' ) ) :
+                if (
+                    get_field( 'black_owned' ) ||
+                    get_field( 'woman_owned' ) ||
+                    get_field( 'latino_owned' )
+                ) :
+                    if ( get_field( 'black_owned' ) ) :
+        ?>
+        <p><i class="fas fa-fw fa-globe-africa"></i> Black Owned Business</p>
+        <?php
+                    endif;
+                    if ( get_field( 'woman_owned' ) ) :
+        ?>
+        <p><i class="fas fa-fw fa-venus"></i> Woman Owned Business</p>
+        <?php
+                    endif;
+                    if ( get_field( 'latino_owned' ) ) :
+        ?>
+        <p><i class="fas fa-fw fa-globe-americas"></i> Latino Owned Business</p>
+        <?php 
+                    endif;
+                else : 
+        ?>
+        <p><i class="fas fa-fw fa-user"></i> Minority Owned Business</p>
+        <?php
+                endif;
+            endif;
+        ?>
     </div>
 </div>
 <?php endif; ?>
@@ -91,8 +126,28 @@ if ( get_field( 'dutchtown_cid' ) || get_field( 'cherokee_street' ) || ( get_fie
             <dd><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></dd>
             <?php endif; ?>
         </dl>
-        <?php 
-        if ( get_field( 'hours' ) ) :
+        <?php
+        if ( get_field( '24_7' ) ) :
+        ?>
+        <h3>Hours</h3>
+        <p>Open 24 hours, seven days a week.</p>
+        <?php
+        elseif ( have_rows( 'daily_hours' ) ) :
+        ?>
+        <h3>Hours</h3>
+        <dl class="place-hours">
+        <?php
+            while ( have_rows( 'daily_hours' ) ) :
+                the_row();
+        ?>
+            <dt><?php the_sub_field( 'day' ); ?></dt>
+            <dd><?php the_sub_field( 'hours' ); ?></dd>
+        <?php
+            endwhile;
+        ?>
+        </dl>
+        <?php
+        elseif ( get_field( 'hours' ) ) :
             $hours = preg_split('/\r\n|\r|\n|: /', get_field( 'hours' ));
         ?>
         <h3>Hours</h3>
@@ -110,13 +165,22 @@ if ( get_field( 'dutchtown_cid' ) || get_field( 'cherokee_street' ) || ( get_fie
                 <?php endif; ?>
             <?php endforeach; ?>
         </dl>
-        <?php endif; ?>
+        <?php
+        endif;
+        if ( get_field( 'address' ) ) :
+        ?>
         <div class="place-map">
             <?php
                 $wp_address = get_field( 'address' ) . ', ' . 'St. Louis, MO ' . get_field( 'zip_code' ); 
-                $shortcode = '[map addr="' . $wp_address .'"]' . get_the_title() . '[/map]';
+                $shortcode = '[map addr="' . $wp_address . '"]';
+                $shortcode .= get_the_title() . ', ';
+                $shortcode .= get_field( 'address' );
+                $shortcode .= '[/map]';
                 echo do_shortcode( $shortcode )
             ?>
         </div>
+        <?php
+            endif;
+        ?>
     </div>
 </section>
